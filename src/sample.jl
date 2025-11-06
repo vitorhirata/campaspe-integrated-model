e459f996e573c895f721d1cb27da1be2f4a2e547
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2025-11-07 08:29:52 +1100

    change default sample parameter

--- src/sample.jl
+++ src/sample.jl
@@ -30,7 +30,7 @@ function sample_individual_options(start_day::String, end_day::String, climate_t
         :farm_step => 14,
         # Policy parameters
         :policy_path => "data/policy",
-        :goulburn_alloc => "high",
+        :goulburn_alloc => "median",
         :restriction_type => "default",
         :max_carryover_perc => 0.25,
         :carryover_period => 1,
@@ -58,6 +58,8 @@ function sample_individual_options(start_day::String, end_day::String, climate_t
     for (i, policy_opt) in enumerate(policy_options)
         scenario[1 + length(farm_options) + i, :policy_option] = policy_opt
     end
+    scenario[12, "farm_option"] = "improve_irrigation_efficiency"
+    scenario[13, "farm_option"] = "implement_solar_panels"
 
-    return scenario
+    return select(scenario, "policy_option", "farm_option" , Not("policy_option", "farm_option"))
 end
